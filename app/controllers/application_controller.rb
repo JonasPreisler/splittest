a712f50775fa78f1c5f5b526eaee250a7817d2b9
jonaspreisler <jonas.preisler@g>
2018-09-27 17:40:27 +0800

    first commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
    before_action :configure_permitted_parameters, if: :devise_controller?
    before_action :set_locale
+   require 'optimizely'
+   require 'httparty'
 
     protected
 
