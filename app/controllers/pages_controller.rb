2c6a5528e5169c20f5e0efc862296ca78a008fba
jonaspreisler <jonas.preisler@g>
2018-09-29 15:10:19 +0800

    first commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,11 @@
 require 'optimizely'
 require 'httparty'
 class PagesController < ApplicationController
-	
+	private
+
 	def landing
 		@variation_key = optimizely.activate('test', 11560980958)
 		url = 'https://cdn.optimizely.com/json/11560980958.json'
 		datafile = HTTParty.get(url).body
-		@variation_key = optimizely.activate('test', 11560980958)
 	end
 end
